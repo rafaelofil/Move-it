3913955a457bbb06359f294b0a505cb27e453e2c
rafael oliveira <rafael.oliveira@helps-on.com>
2021-02-24 15:26:27 -0300

    Fix document file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,7 @@ import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return <div className={styles.container}> 
-    <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+    <Head>       
       <title>Início | move-it</title>
     </Head>
     <ExperienceBar />
