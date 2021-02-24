4d40291e926e20b92a0429b6b9062432facc1d07
rafael oliveira <rafael.oliveira@helps-on.com>
2021-02-24 18:37:54 -0300

    Challenges Box

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,49 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  return (
-    <div className="styles.challengeBoxContainer">
+  const contextData = useContext(ChallengesContext);
+  console.log(contextData);
 
+  const hasActiveChallenge = true;
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      { hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg"/>
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+
+          <footer>
+            <button 
+              type="button"
+              className={styles.challengeFailedButton}
+            >
+              Falhei</button>
+            <button 
+              type="button"
+              className={styles.challengeSuceededButton}
+            >
+              Completei</button>
+          </footer>
+        </div>
+      ) : (
+      <div className={styles.challengeNotActive}>
+        <strong>Finalize um ciclo para receber um desafio</strong>
+        <p>
+          <img src="icons/level-up.svg" alt="Level Up"/>
+          Avance de level completando desafios.
+        </p>
+      </div>
+      )}
+      
     </div>
   );
 }
