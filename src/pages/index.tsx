164c3208fc56b8df140c50fda00b6963d78c0728
rafael oliveira <rafael.oliveira@helps-on.com>
2021-02-25 16:10:54 -0300

    Level countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { Profile } from "../components/Profile";
 import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 export default function Home() {
   return <div className={styles.container}> 
@@ -15,15 +16,17 @@ export default function Home() {
     </Head>
     <ExperienceBar />
 
-    <section>
-      <div>
-        <Profile />
-        <CompletedChallenges />
-        <Countdown />
-      </div>
-      <div>
-        <ChallengeBox />
-      </div>
-    </section>
+    <CountdownProvider>
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+          <ChallengeBox />
+        </div>
+      </section>
+    </CountdownProvider>
   </div>;
 }
